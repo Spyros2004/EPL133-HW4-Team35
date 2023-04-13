3170bd9c412583f910d6e28451b087d8d700aa69
Spyros Sachmpazidis <spyros.s2004@gmail.com>
2023-04-13 14:33:46 +0300

    Classes that will be used(will be updated in the future if needed)

---
+++ HW4/src/hw4/SentenceGenerator.java
@@ -0,0 +1,10 @@
+package hw4;
+
+public class SentenceGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HW4/src/hw4/TextGenerator.java
@@ -0,0 +1,10 @@
+package hw4;
+
+public class TextGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
