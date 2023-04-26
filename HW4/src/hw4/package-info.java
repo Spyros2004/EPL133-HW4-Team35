fa89c7d7d33868527975a4058c7ffab1bb969f09
Spyros Sachmpazidis <spyros.s2004@gmail.com>
2023-04-26 20:53:00 +0300

    Added package-info.java

--- HW4/src/hw4/package-info.java
+++ HW4/src/hw4/package-info.java
@@ -1,35 +1,34 @@
-package hw4;
-
 /**
  * This package contains the necessary classes to run make a program answer the
  * questions in a synonym quiz. The program first needs to get trained with a
  * lot of text. After that it can answer synonym questions based on an algorithm
  * that uses semantic descriptor vectors to find the most likely synonym.
  * 
- * The package contains 8 classes: {@link MostSimilarWord}
- * ,{@link SemanticDescriptors},
- * {@link SentenceGenerator},{@link SysnonymQueston},{@link SysnonymQuiz},{@link TextGenerator},{@link WordGenerator},
- * and the test class {@link SysnonymQuizTest}.
+ * The package contains 8 classes: MostSimilarWord ,SemanticDescriptors,
+ * SentenceGenerator,SysnonymQueston,SysnonymQuiz,TextGenerator,WordGenerator,
+ * and the test class SysnonymQuizTest.
  *
- * The class {@link MostSimilarWord} uses semantic descriptors to find the
+ * The class MostSimilarWord uses semantic descriptors to find the
  * likely synonym of a word from a list of choices. The synonym is picked based
  * on what word from the choices appears in similar sentences with the question
  * word.
  * 
- * The class {@link SemanticDescriptors} creates a 2D Hash map with the semantic
+ * The class SemanticDescriptors creates a 2D Hash map with the semantic
  * descriptors from a a list of sentences.
  * 
- * The class {@link SentenceGenerator} creates a list of sentences, each of
+ * The class SentenceGenerator creates a list of sentences, each of
  * which contains a list of the words inside the sentences.
  * 
- * The class {@link SysnonymQueston} represents a question in a synonym quiz and
+ * The class SysnonymQueston represents a question in a synonym quiz and
  * contains a question word,the answer and the list of choices.
  * 
- * The class {@link SysnonymQuiz} represents a synonym quiz and contains synonym
+ * The class SysnonymQuiz represents a synonym quiz and contains synonym
  * questions.
  * 
- * The class {@link TexteGenerator} returns the text from inside a file.
+ * The class TexteGenerator returns the text from inside a file.
  * 
- * The class {@link WordGenerator} creates a list of words form a sentence.
+ * The class WordGenerator creates a list of words form a sentence.
  * 
  */
+
+package hw4;
