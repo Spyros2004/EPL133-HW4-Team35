9ce589fad7497aa3a7bb83ea08fa8aaa0646fe2e
Spyros Sachmpazidis <spyros.s2004@gmail.com>
2023-04-26 21:10:38 +0300

    Finishing touches

--- HW4/src/hw4/SynonymQuiz.java
+++ HW4/src/hw4/SynonymQuiz.java
@@ -6,26 +6,30 @@ import java.util.List;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.FileOutputStream;
+
 /**
- * The SynonymQuiz class represents a synonym quiz with a list of synonym questions.
+ * The SynonymQuiz class represents a synonym quiz with a list of synonym
+ * questions.
+ * 
  * @author kkypri06 , ssachm02
  * @version 1.1
  * @since 10/04/22
  *
  */
 public class SynonymQuiz {
-	
+
 	/**
 	 * This is the list of {@link SynonymQuestion} in the quiz.
 	 * 
 	 * @since 1.0
 	 */
 	private List<SynonymQuestion> questions;
-	
+
 	/**
-	 * Constructs a new SynonymQuiz object from a given file. 
-	 * Each line of the file contain a word followed by a list 
-	 * of choices and then the answer (choices for the quiz). 
+	 * Constructs a new SynonymQuiz object from a given file. Each line of the file
+	 * contain a word followed by a list of choices and then the answer (choices for
+	 * the quiz).
+	 * 
 	 * @param filename the name of the file to read from
 	 * @since 1.0
 	 */
@@ -50,12 +54,14 @@ public class SynonymQuiz {
 		}
 	}
 
-	
 	/**
-	 * Runs a similarity test on the quiz questions using MostSimilarWord and 
-	 * the semantic descriptors we created at SemanticDescriptors.
-	 * @param filename the name of the file to read the quiz questions from
-	 * @param semantic_descriptors the HashMap of semantic descriptors to use in the similarity calculation
+	 * Runs a similarity test on the quiz questions using MostSimilarWord and the
+	 * semantic descriptors we created at SemanticDescriptors.
+	 * 
+	 * @param filename             the name of the file to read the quiz questions
+	 *                             from
+	 * @param semantic_descriptors the HashMap of semantic descriptors to use in the
+	 *                             similarity calculation
 	 * @return the percentage of correct answers in the test
 	 * @since 1.0
 	 */
@@ -80,12 +86,13 @@ public class SynonymQuiz {
 		}
 		outputStream.close();
 		double percentage = (double) counter / quiz.getQuestionCount();
+		System.out.print("Success rate is : " + Math.round(percentage * 100) + '%');
 		return percentage;
 	}
-	
-	
+
 	/**
 	 * Returns the SynonymQuestion object at the specified index in the quiz.
+	 * 
 	 * @param index the index of the question to return
 	 * @return the SynonymQuestion object at the specified index
 	 * @since 1.0
@@ -94,9 +101,9 @@ public class SynonymQuiz {
 		return questions.get(index);
 	}
 
-	
 	/**
 	 * Returns the number of questions in the quiz.
+	 * 
 	 * @return the number of questions in the quiz
 	 * @since 1.0
 	 */
--- HW4/src/hw4/SynonymQuizTest.java
+++ HW4/src/hw4/SynonymQuizTest.java
@@ -20,14 +20,13 @@ public class SynonymQuizTest {
 	 */
 	public static void main(String[] args) {
 		List<String> filenames = new ArrayList<String>();
-		filenames.add("C:\\users\\spyro\\Desktop\\pg2600.txt");
-		filenames.add("C:\\users\\spyro\\Desktop\\pg7178.txt");
-		filenames.add("C:\\users\\spyro\\Desktop\\brown-train-sentences.txt");
+		filenames.add("pg2600.txt");
+		filenames.add("pg7178.txt");
+		filenames.add("brown-train-sentences.txt");
 		List<List<String>> sentences = SentenceGenerator.getSentenceListsFromFiles(filenames);
 		HashMap<String, HashMap<String, Integer>> semanticDescriptors = SemanticDescriptors
 				.createSemanticDescriptors(sentences);
-		double a = SynonymQuiz.runSimilarityTest("C:\\users\\spyro\\Desktop\\questions.txt", semanticDescriptors);
-		System.out.println("Success rate: " + a);
+		SynonymQuiz.runSimilarityTest("questions.txt", semanticDescriptors);
 	}
 
 }
